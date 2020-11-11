79a0fc94bda54f4cc8eb8533eec3cc6c608d6f22
Artur Zurek <artur.zurek@ssginsight.com>
2020-11-11 12:41:12 +0100

    Removed installing dependencies in postinstall script.

--- postinstall.cmd
+++ postinstall.cmd
@@ -1,7 +1,7 @@
 (if not exist .\node-red-data mkdir .\node-red-data)
 copy npm_.cmd .\node-red-data\npm_.cmd
 pushd .\node-red-data
-npm install node-red-dashboard node-red-contrib-crypto-js-mod node-red-contrib-full-msg-json-schema-validation node-red-contrib-uuid node-red-node-base64 node-red-node-mongodb & (
+npm install & (
      copy npm_.cmd npm.cmd /y
      popd 
      copy npm_.cmd npm.cmd /y
