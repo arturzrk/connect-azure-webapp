4eb307c5f164a0e4bf7f8dd32e39d9a207e5dc7c
Artur Zurek <artur.zurek@ssginsight.com>
2020-11-11 11:19:59 +0100

    Updated postinstall.cmd to reflect new folder structure.

--- postinstall.cmd
+++ postinstall.cmd
@@ -1,8 +1,6 @@
-(if not exist \home mkdir \home)
-(if not exist \home\nodered mkdir \home\nodered)
-(if not exist \home\nodered\.node-red mkdir \home\nodered\.node-red)
-copy npm_.cmd \home\nodered\.node-red\npm_.cmd
-pushd \home\nodered\.node-red
+(if not exist .\node-red-data mkdir .\node-red-data)
+copy npm_.cmd .\node-red-data\npm_.cmd
+pushd .\node-red-data
 npm install node-red-dashboard node-red-contrib-crypto-js-mod node-red-contrib-full-msg-json-schema-validation node-red-contrib-uuid node-red-node-base64 node-red-node-mongodb & (
      copy npm_.cmd npm.cmd /y
      popd 
